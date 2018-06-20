caa403d44d980b065289e5a5c7be6f38f293d9c7
Marta <marta.rose@gmail.com>
2018-06-19 22:52:42 -0700

    updated as turned in

--- views.py
+++ views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from django.utils.crypto import get_random_string 
 # Create your views here.
 def index(request):
+    if not 'counter' in request.session:
+        request.session['counter'] = 1
+    else:
+        request.session['counter'] += 1
+    
+    word = get_random_string(length=14)
     context= {
-        "word": get_random_string(length=14),
-        "number": (number + 1)
+        "word": word
     }
     return render(request, "random_word/index.html", context)
